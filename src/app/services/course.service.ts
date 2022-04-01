cd3ef3c4bfa0a2f2e7a106b9f9bc4e14762344ca
Ndoda Kheswa <NKheswa2@oldmutual.com>
2022-04-01 13:50:05 +0200

    changed api endpoints for course service

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -14,21 +14,21 @@ export class CourseService {
   constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) { this.apiUrl = baseUrl }
 
   getAll(): Observable<Course[]> {
-    return this.http.get<Course[]>(this.apiUrl + '/user');
+    return this.http.get<Course[]>(this.apiUrl + '/course');
   }
 
   getUserCourses(id: string): Observable<Course[]> {
-    return this.http.get<Course[]>(this.apiUrl + `/user/courses/${id}`);
+    return this.http.get<Course[]>(this.apiUrl + `/course/user/${id}`);
   }
 
   getCourse(id: string): Observable<Course[]> {
-    return this.http.get<Course[]>(this.apiUrl + `/user/course/${id}`);
+    return this.http.get<Course[]>(this.apiUrl + `/course/${id}`);
   }
 
 
   enrollUser(userId: string, courseId: string) {
 
-    return this.http.post<any>(this.apiUrl + `/user/assignCourse`, {"userId": userId, "courseId": courseId})
+    return this.http.post<any>(this.apiUrl + `/course/assign/user`, {"userId": userId, "courseId": courseId})
   }
 
 }
